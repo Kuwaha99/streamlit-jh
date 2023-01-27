8111a7ac09e6604adc6a8cc7e24d233ca1807b0c
Kuwaha99 <119403834+Kuwaha99@users.noreply.github.com>
2023-01-27 17:00:08 +0900

    Update jhgit.py

--- jhgit.py
+++ jhgit.py
@@ -59,8 +59,9 @@ with st.form(key ='Form1'):
         select_language = st.sidebar.radio('What do you want :red[graph] ?', ("line graph","bar graph","pie graph","corr graph","word cloud","box graph" ))
         
         
-if select_language =="line graph":        
+if select_language =="line graph":
+   
     try:
-        plotting_demo()
-    except:
-        pass
+          plotting_demo()  
+    except:      
+          pass
