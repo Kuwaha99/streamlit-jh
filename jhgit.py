2e7f86c22c7d7bfa03a2eb11c5d2db166361692f
Kuwaha99 <119403834+Kuwaha99@users.noreply.github.com>
2023-02-02 16:14:54 +0900

    Update jhgit.py

--- jhgit.py
+++ jhgit.py
@@ -22,6 +22,11 @@ def  plotting_demo():
     option2 = int(option)
     st.write('You selected:', option)
     money = money[:] [money['A_YEAR']== option2]
+    
+    global  aa
+    aa = money      
+      
+      
     fig, ax = plt.subplots(2,2, figsize=(12,8))
     plt.subplot(221)
     plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
@@ -42,7 +47,7 @@ def  plotting_demo():
     
    
     st.pyplot(fig)
-    st.dataframe(money)
+    #st.dataframe(money)
    
 def bar_chart():
     
@@ -82,7 +87,15 @@ with st.form(key ='Form1'):
 
 
 if select_language =='금리와 집값':
-   plotting_demo()  
+      tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+   
+    with tab1:
+        tab1.subheader("A tab with a chart")
+        plotting_demo()
+        
+    with tab2:
+        tab2.subheader("A tab with the data")
+        st.dataframe(aa)
    
 
 elif select_language =='야구 순위와 승률':
